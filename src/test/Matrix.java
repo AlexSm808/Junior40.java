4c10fc8c011d46c6bf89f6c9a3e282150077101e
AlexSm808 <9057097288@mail.ru>
2023-10-14 22:32:44 +0300

    обновлен тест

--- src/test/Matrix.java
+++ src/test/Matrix.java
@@ -1,5 +1,7 @@
 package test;
 
+import junit.framework.TestCase;
+
 public class Matrix implements IMatrix {
 
     private double[][] nums;
@@ -217,5 +219,25 @@ public class Matrix implements IMatrix {
             System.out.print("\n");
         }
         System.out.println();
+    }
+
+    public static class MatrixTestCase extends TestCase {
+
+        public void testDeterminantTest1() {
+
+            double[][] forMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+            Matrix matrix = new Matrix(forMatrix);
+            double resultFact = matrix.determinant();
+            double resultWant = 0.0;
+            assertEquals(resultFact, resultWant);
+        }
+        public void testDeterminantTest2() {
+
+            double[][] forMatrix = {{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
+            Matrix matrix = new Matrix(forMatrix);
+            double resultFact = matrix.determinant();
+            double resultWant = 0.0;
+            assertEquals(resultFact, resultWant);
+        }
     }
 }
