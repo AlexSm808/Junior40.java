16c6d34b37456db8196b420882d11b5df45c3503
AlexSm808 <9057097288@mail.ru>
2023-10-15 11:03:09 +0300

    add worked TEST

---
+++ src/test/MatrixTestCase.java
@@ -0,0 +1,23 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class MatrixTestCase extends TestCase {
+
+    public void testDeterminantTest1() {
+
+        double[][] forMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Matrix matrix = new Matrix(forMatrix);
+        double resultFact = matrix.determinant();
+        double resultWant = 0.0;
+        assertEquals(resultFact, resultWant);
+    }
+    public void testDeterminantTest2() {
+
+        double[][] forMatrix = {{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
+        Matrix matrix = new Matrix(forMatrix);
+        double resultFact = matrix.determinant();
+        double resultWant = 0.0;
+        assertEquals(resultFact, resultWant);
+    }
+}
