05ca6d5baad87ae0dbfc162894cdeaddd56d6cda
AlexSm808 <9057097288@mail.ru>
2023-10-20 00:28:40 +0300

    Добавлены три метода по копированию файлов

--- src/lesson15/Test.java
+++ src/lesson15/Test.java
@@ -12,12 +12,32 @@ public class Test {
         long time3 = System.currentTimeMillis();
         copywithGuava.copy("C:\\Users\\Пользователь\\IdeaProjects\\javaJunior40\\src\\lesson1\\SW.BAND.zip", "test2.zip");
         long time4 = System.currentTimeMillis();
-        System.out.println("Копирование по одному байту: " + (time4 - time3));
+        System.out.println("Копирование Guava: " + (time4 - time3));
 
         CopyWithCommonIO copyWithCommonIO = new CopyWithCommonIO();
         long time5 = System.currentTimeMillis();
         copyWithCommonIO.copy("C:\\Users\\Пользователь\\IdeaProjects\\javaJunior40\\src\\lesson1\\SW.BAND.zip", "test3.zip");
         long time6 = System.currentTimeMillis();
-        System.out.println("Копирование по одному байту: " + (time6 - time5));
+        System.out.println("Копирование CommonIO: " + (time6 - time5));
+
+        CopyWithNIO copyWithNIO = new CopyWithNIO();
+        long time7 = System.currentTimeMillis();
+        copyWithNIO.copy("C:\\Users\\Пользователь\\IdeaProjects\\javaJunior40\\src\\lesson1\\SW.BAND.zip", "test4.zip");
+        long time8 = System.currentTimeMillis();
+        System.out.println("Копирование NIO: " + (time8 - time7));
+
+        CopyWithChannels copyWithChannels = new CopyWithChannels();
+        long time9 = System.currentTimeMillis();
+        copyWithChannels.copy("C:\\Users\\Пользователь\\IdeaProjects\\javaJunior40\\src\\lesson1\\SW.BAND.zip", "test5.zip");
+        long time10 = System.currentTimeMillis();
+        System.out.println("Копирование Channels: " + (time10 - time9));
+
+        CopyWithStreams copyWithStreams = new CopyWithStreams();
+        long time11 = System.currentTimeMillis();
+        copyWithStreams.copy("C:\\Users\\Пользователь\\IdeaProjects\\javaJunior40\\src\\lesson1\\SW.BAND.zip", "test6.zip");
+        long time12 = System.currentTimeMillis();
+        System.out.println("Копирование c помощью потоков " + (time12 - time11));
+
+
     }
 }
